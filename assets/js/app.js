3cacf07d59da0fad48602ece936757e41d234091
Huseyn-Ismayilov <info.cyweb@gmail.com>
2023-11-07 14:10:52 +0400

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,44 @@
+$(window).scroll(function () {
+	var scroll = $(window).scrollTop();
+
+	if (scroll >= 60) {
+		$(".site_header").addClass("scroll_down");
+	} else {
+		$(".site_header").removeClass("scroll_down");
+	}
+});
+
+
+
+$('.site_header .toggle, .mobile_menu .close_btn').click(function () {
+	$('.mobile_menu').toggleClass('opened');
+	$('.nav_menu').toggleClass('opened');
+	$('.site_header .toggle').toggleClass('opened')
+});
+
+$('.mobile_menu .menu .dropdown .nav_link').click(function (e) {
+	$(this).next().toggleClass('opened');
+	return false;
+});
+$('.mobile_menu .menu .back_btn').click(function (e) {
+	$(this).parent().removeClass('opened');
+});
+
+// end
+
+
+// end
+$(document).click(function (event) {
+	if (!$(event.target).closest(".site_header .toggle, .mobile_menu .inner").length) {
+		$("body").find(".mobile_menu .inner").parent().removeClass("opened");
+		$('.site_header .toggle').removeClass('opened');
+	}
+});
+
+
+
+$('.select_language .btn').click(function () {
+	$(this).toggleClass('active');
+	$(this).next().slideToggle()
+});
+
